d589918809470ee4a13d2085930de77622b6415f
Raef Coles <raef@rccoles.dev>
2020-11-17 20:59:49 +0000

    Add caching of updated gpx
    Also, add the relative path of the cached file to the output json

--- map_util.py
+++ map_util.py
@@ -4,8 +4,12 @@ import srtm
 import polyline
 import json
 import numpy as np
+import os
 
 elevation_data = srtm.get_data(local_cache_dir="srtm_cache")
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+print(base_dir)
 
 def process_gpx(gpx_filepath, height_max_len=100):
     out_dict = {}
@@ -14,10 +18,18 @@ def process_gpx(gpx_filepath, height_max_len=100):
         gpx = gpxpy.parse(gpx_file)
 
     elevation_data.add_elevations(gpx)
+    gpx.remove_time()
+
+    gpx_filename = os.path.basename(gpx_filepath)
+    new_gpx_filepath = os.path.join(base_dir, "gpx_cache", gpx_filename)
+    os.makedirs(os.path.dirname(new_gpx_filepath), exist_ok=True)
+    with open(new_gpx_filepath, "wt") as new_gpx_file:
+        new_gpx_file.write(gpx.to_xml())
 
     out_dict['name'] = gpx.name
     out_dict['description'] = gpx.description
     out_dict['routes'] = []
+    out_dict['path'] = os.path.relpath(new_gpx_filepath, base_dir)
 
     for route in gpx.routes + gpx.tracks:
         route.remove_time()
@@ -52,6 +64,6 @@ if __name__ == "__main__":
 
     output = []
     for gpx in gpxs:
-        output.append(process_gpx(gpx))
+        output.append(process_gpx(os.path.abspath(gpx)))
 
     print(json.dumps(output))
