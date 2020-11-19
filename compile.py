5311cac35974ce38923354680709aa58b9b804d9
Raef Coles <raef@rccoles.dev>
2020-11-19 19:18:42 +0000

    Update directory structure
    Make output into a single json file
    
    Signed-off-by: Raef Coles <raef@rccoles.dev>

--- compile.py
+++ compile.py
@@ -9,6 +9,8 @@ import hashlib
 elevation_data = srtm.get_data(local_cache_dir='srtm_cache')
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
+gpx_dir  = os.path.join(base_dir, "gpx")
+data_dir = os.path.join(base_dir, "data")
 
 def process_gpx(gpx_filepath, height_max_len=100):
     out_dict = {}
@@ -43,14 +45,14 @@ def process_gpx(gpx_filepath, height_max_len=100):
         out_dict['routes'].append(out_route)
 
     output_filename = hashlib.sha1(json.dumps(out_dict['routes']).encode('UTF-8')).hexdigest()[10:]
-    out_dict['filename'] = os.path.join('gpx_cache', output_filename + '.gpx')
+    out_dict['filename'] = os.path.join('gpx', output_filename + '.gpx')
 
     new_gpx_filepath = os.path.join(base_dir, out_dict['filename'])
     os.makedirs(os.path.dirname(new_gpx_filepath), exist_ok=True)
     with open(new_gpx_filepath, 'wt') as new_gpx_file:
         new_gpx_file.write(gpx.to_xml())
 
-    return out_dict, output_filename
+    return out_dict
 
 if __name__ == '__main__':
     description = 'Process multiple gpx files into a single polyline file'
@@ -60,29 +62,19 @@ if __name__ == '__main__':
                         help='A list of gpx files to process')
 
     args = parser.parse_args()
+
     gpxs = args.gpxs
+    gpxs += [os.path.join(gpx_dir, gpx) for gpx in os.listdir(gpx_dir)]
 
-    gpxs += [os.path.join(base_dir, 'gpx_cache', gpx) for gpx in
-             os.listdir((os.path.join(base_dir, 'gpx_cache')))]
-
-    data_dir = os.path.join(base_dir, 'data')
     os.makedirs(data_dir, exist_ok=True)
+    output = []
 
     for gpx in gpxs:
-        gpx_filepath = os.path.abspath(gpx)
+        output.append(process_gpx(gpx))
 
-        output, output_filename = process_gpx(gpx_filepath)
-        output = json.dumps(output).encode('UTF-8')
+    output = json.dumps(output).encode('UTF-8')
 
-        output_filepath = os.path.join(data_dir, output_filename + '.json')
-        with open(output_filepath, 'wb') as output_file:
-            output_file.write(output)
-
-
-    manifest_filepath = os.path.join(base_dir, 'manifest.txt')
-
-    with open(manifest_filepath, 'wt') as manifest_file:
-        manifest = [ os.path.join('data',filename) + '\n' for filename in os.listdir(data_dir)]
-        manifest_file.writelines(manifest)
-
+    output_filepath = os.path.join(base_dir, 'generated', 'data.json')
+    with open(output_filepath, 'wb') as output_file:
+        output_file.write(output)
 
